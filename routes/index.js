b40b454bf72089dd2d8a332b63f7f12e01016f91
jason <jasonkwm13@gmail.com>
2017-11-02 18:33:11 +0800

    complete

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,31 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get("/:time",(req,res)=>{
+  function unixToNatural(unix){
+    let date = new Date(unix * 1000);
+    let months = ["January", "Febuary","March","April","May","June","July","August","September","October", "November","December"];
+    let month = months[date.getMonth()];
+    let day = date.getDate();
+    let year = date.getFullYear();
+
+    let result = month + " " + day + ", "+year;
+    return result;
+  }
+  if(!isNaN(req.params.time)){
+    let result = unixToNatural(req.params.time);
+    let data = {unix: req.params.time, natural: result};
+    res.json(data)
+  } else {
+    let natural = new Date(req.params.time);
+    if(!isNaN(natural)){
+      let unix = natural / 1000;
+      let data = {unix: unix, natural: req.params.time };
+      res.json(data)
+    } else{
+      res.json({unix: null, natural: null})
+    }
+  }
+})
+
 module.exports = router;
